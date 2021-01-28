502afbd6639dc7a839cedb35353d0b0af07b60ed
Tobias Richter <tr4711@gmail.com>
2021-01-28 21:46:13 +0100

    Add processing function

--- cf_benchmark/data/processing.py
+++ cf_benchmark/data/processing.py
@@ -1,3 +1,20 @@
-def normalize(data, scale=None):
-    # TODO: normalize data
-    return data
+from sklearn import preprocessing
+
+
+def normalize(df, num_cols, scaler=None):
+    """
+    Normalizes continous columns of the given dataframe
+
+    :param df: Datafame to scale
+    :param num_cols: Numerical columns to scale
+    :param scaler: Prefitted Sklearn Scaler
+    :return: Data
+    """
+
+    if not scaler:
+        scaler = preprocessing.MinMaxScaler()
+        scaler.fit(df[num_cols])
+
+    df[num_cols] = scaler.transform(df[num_cols])
+
+    return df
