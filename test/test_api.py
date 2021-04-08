a90d82e04f3131c9d397659580acb411265fe0f6
Sascha <sascha.bielawski@student.uni-tuebingen.de>
2021-04-08 11:19:04 +0200

    New tests for data api and adult dataset

---
+++ test/test_api.py
@@ -0,0 +1,13 @@
+from carla.data.catalog import DataCatalog
+from carla.data.api import Data
+from abc import ABC
+
+
+def test_data():
+    data_name = "adult"
+    data_catalog = "adult_catalog.yaml"
+    data_catalog = DataCatalog(data_name, data_catalog)
+
+    assert issubclass(DataCatalog, Data)
+    assert isinstance(data_catalog, Data)
+    assert issubclass(Data, ABC)
