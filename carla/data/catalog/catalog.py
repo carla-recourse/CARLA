219a7c31375a1874960d364401627da9387ff909
Philoso-Fish <sbielawski@web.de>
2021-04-16 09:14:08 +0200

    Api data (#14)
    * ignore pycharm working directories
    
    * Implementing abstract data class to allow arbitrary datasets.
    Restructuring data folder given new abstract class.
    
    * New tests for data api and adult dataset
    
    * isort for failing check
    
    * Changes for merging in PR Api data #14
    
    Co-authored-by: Sascha <sascha.bielawski@student.uni-tuebingen.de>

--- carla/data/catalog/catalog.py
+++ carla/data/catalog/catalog.py
@@ -1,11 +1,12 @@
 import pandas as pd
 import yaml
 
-from . import processing
+from ..api import Data
+from ..processing import processing
 from .load_data import load_dataset
 
 
-class DataCatalog:
+class DataCatalog(Data):
     def __init__(self, data_name, catalog_file):
         self.name = data_name
         self.catalog = self._load_catalog(catalog_file, data_name)
@@ -34,21 +35,21 @@ class DataCatalog:
 
     @property
     def raw(self):
-        return self._raw
+        return self._raw.copy()
 
     @property
     def normalized(self):
         if self._data_normalized is None:
             self._data_normalized = processing.normalize(self.raw, self.continous)
 
-        return self._data_normalized
+        return self._data_normalized.copy()
 
     @property
     def encoded(self):
         if self._data_encoded is None:
             self._data_encoded = pd.get_dummies(self.raw, drop_first=True)
 
-        return self._data_encoded
+        return self._data_encoded.copy()
 
     @property
     def encoded_normalized(self):
@@ -57,7 +58,7 @@ class DataCatalog:
                 self.encoded, self.continous
             )
 
-        return self._data_encoded_normalized
+        return self._data_encoded_normalized.copy()
 
     def _load_catalog(self, filename, dataset):
         with open(filename, "r") as f:
---
+++ test/test_data.py
@@ -0,0 +1,57 @@
+import numpy as np
+
+from carla.data.catalog import DataCatalog
+
+
+def test_adult_col():
+    data_name = "adult"
+    data_catalog = "adult_catalog.yaml"
+    data_catalog = DataCatalog(data_name, data_catalog)
+
+    actual_col = (
+        data_catalog.categoricals + data_catalog.continous + [data_catalog.target]
+    )
+    actual_col = actual_col.sort()
+    expected_col = data_catalog.raw.columns.values
+    expected_col = expected_col.sort()
+
+    assert actual_col == expected_col
+
+
+def test_adult_norm():
+    data_name = "adult"
+    data_catalog = "adult_catalog.yaml"
+    data_catalog = DataCatalog(data_name, data_catalog)
+
+    col = data_catalog.continous
+
+    raw = data_catalog.raw[col]
+    norm = data_catalog.normalized[col]
+
+    assert ((raw != norm).all()).any()
+
+
+def test_adult_enc():
+    data_name = "adult"
+    data_catalog = "adult_catalog.yaml"
+    data_catalog = DataCatalog(data_name, data_catalog)
+    cat = data_catalog.encoded
+
+    assert cat.select_dtypes(exclude=[np.number]).empty
+
+
+def test_adult_norm_enc():
+    data_name = "adult"
+    data_catalog = "adult_catalog.yaml"
+    data_catalog = DataCatalog(data_name, data_catalog)
+
+    norm_col = data_catalog.continous
+    norm_enc_col = data_catalog.encoded_normalized.columns
+
+    cat = data_catalog.encoded
+    cat[norm_col] = data_catalog.normalized[norm_col]
+    cat = cat[norm_enc_col]
+
+    cat_norm = data_catalog.encoded_normalized
+
+    assert ((cat_norm == cat).all()).all()
