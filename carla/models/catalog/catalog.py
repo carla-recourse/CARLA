0891a2c43443b3205c98efe0674724658bb1cbae
Sascha <sascha.bielawski@student.uni-tuebingen.de>
2021-04-21 14:47:04 +0200

    Improvements commented in PR Api model #17

--- carla/models/catalog/catalog.py
+++ carla/models/catalog/catalog.py
@@ -6,19 +6,47 @@ from .load_model import load_model
 
 class MLModelCatalog(MLModel):
     def __init__(
-        self, data_name, model_type, ext="h5", cache=True, models_home=None, **kws
+        self,
+        data_name,
+        model_type,
+        backend="tensorflow",
+        cache=True,
+        models_home=None,
+        **kws
     ):
-        self._data_name = data_name
-        self._model = load_model(model_type, data_name, ext, cache, models_home, **kws)
+        """
+        Constructor for pretrained ML models from the catalog.
 
-        if ext == "pt":
-            self._backend = "pytorch"
-        elif ext == "h5":
-            self._backend = "tensorflow"
+        Possible backends are currently "pytorch" and "tensorflow".
+        Possible models are corrently "ann".
+
+        Parameters
+        ----------
+        data_name : str
+            Name of the dataset which is used to predict [adult]
+        model_type : str
+            Architecture [ann]
+        backend : str
+            Specifies the used framework [tensorflow, pytorch]
+        cache : boolean, optional
+            If True, try to load from the local cache first, and save to the cache
+            if a download is required.
+        models_home : string, optional
+            The directory in which to cache data; see :func:`get_models_home`.
+        kws : keys and values, optional
+            Additional keyword arguments are passed to passed through to the read model function
+        """
+        self._backend = backend
+
+        if self._backend == "pytorch":
+            ext = "pt"
+        elif self._backend == "tensorflow":
+            ext = "h5"
         else:
             raise Exception("Model type not in catalog")
 
-        self._name = model_type + "_" + data_name
+        self._data_name = data_name
+        self._model = load_model(model_type, data_name, ext, cache, models_home, **kws)
 
         if data_name == "adult":
             self._feature_input_order = [
@@ -38,18 +66,6 @@ class MLModelCatalog(MLModel):
             ]
         else:
             raise Exception("Model for dataset not in catalog")
-
-    @property
-    def name(self):
-        """
-        Contains meta information about the model, like name, dataset, structure, etc.
-
-        Returns
-        -------
-        name : String
-            Individual name of the ml model
-        """
-        return self._name
 
     @property
     def feature_input_order(self):
@@ -108,18 +124,21 @@ class MLModelCatalog(MLModel):
             Ml model prediction for interval [0, 1] with shape N x 1
         """
 
-        assert len(x.shape) == 2
+        if len(x.shape) != 2:
+            raise ValueError("Input shape has to be two-dimensional")
 
         if self._backend == "pytorch":
-            output = self._model.predict(x)
+            return self._model.predict(x)
         elif self._backend == "tensorflow":
-            output = self._model.predict(x)[:, 1]
-
-        return output
+            return self._model.predict(x)[:, 1]
+        else:
+            raise ValueError(
+                'Uncorrect backend value. Please use only "pytorch" or "tensorflow".'
+            )
 
     def predict_proba(self, x):
         """
-        Two-dimensional softmax prediction of ml model
+        Two-dimensional probability prediction of ml model
 
         Shape of input dimension has to be always two-dimensional (e.g., (1, m), (n, m))
 
@@ -134,7 +153,8 @@ class MLModelCatalog(MLModel):
             Ml model prediction with shape N x 2
         """
 
-        assert len(x.shape) == 2
+        if len(x.shape) != 2:
+            raise ValueError("Input shape has to be two-dimensional")
 
         if self._backend == "pytorch":
             class_1 = 1 - self._model.forward(x).detach().numpy().squeeze()
@@ -146,9 +166,11 @@ class MLModelCatalog(MLModel):
                 class_1 = np.array(class_1).reshape(1)
                 class_2 = class_2.reshape(1)
 
-            output = np.array(list(zip(class_1, class_2)))
+            return np.array(list(zip(class_1, class_2)))
 
         elif self._backend == "tensorflow":
-            output = self._model.predict(x)
-
-        return output
+            return self._model.predict(x)
+        else:
+            raise ValueError(
+                'Uncorrect backend value. Please use only "pytorch" or "tensorflow".'
+            )
--- test/test_mlmodel.py
+++ test/test_mlmodel.py
@@ -5,12 +5,8 @@ from carla.models.catalog import MLModelCatalog
 def test_properties():
     data_name = "adult"
     model_tf_adult = MLModelCatalog(data_name, "ann")
-    # TODO: Issue #16
-    # model_pt_adult = MLModelCatalog(data_name, "ann", ext="pt")
 
     exp_backend_tf = "tensorflow"
-    # TODO: Issue #16
-    # exp_backend_pt = "pytorch"
     exp_feature_order_adult = [
         "age",
         "fnlwgt",
@@ -28,11 +24,7 @@ def test_properties():
     ]
 
     assert model_tf_adult.backend == exp_backend_tf
-    # TODO: Issue #16
-    # assert model_pt_adult.backend == exp_backend_pt
     assert model_tf_adult.feature_input_order == exp_feature_order_adult
-    # TODO: Issue #16
-    # assert model_pt_adult.feature_input_order == exp_feature_order_adult
 
 
 def test_predictions():
@@ -41,8 +33,6 @@ def test_predictions():
     data = DataCatalog(data_name, data_catalog)
 
     model_tf_adult = MLModelCatalog(data_name, "ann")
-    # TODO: Issue #16
-    # model_pt_adult = MLModelCatalog(data_name, "ann", ext="pt")
 
     single_sample = data.encoded_normalized.iloc[22]
     single_sample = single_sample[model_tf_adult.feature_input_order].values.reshape(
@@ -56,31 +46,15 @@ def test_predictions():
     expected_shape = tuple((1,))
     assert single_prediction_tf.shape == expected_shape
 
-    # TODO: Issue #16
-    # single_prediction_pt = model_pt_adult.predict(single_sample)
-    # assert single_prediction_pt.shape == expected_shape
-
     predictions_tf = model_tf_adult.predict(samples)
     expected_shape = tuple((22,))
     assert predictions_tf.shape == expected_shape
-
-    # TODO: Issue #16
-    # predictions_pt = model_pt_adult.predict(samples)
-    # assert predictions_pt.shape == expected_shape
 
     # Test single and bulk probabilistic predictions
     single_predict_proba_tf = model_tf_adult.predict_proba(single_sample)
     expected_shape = tuple((1, 2))
     assert single_predict_proba_tf.shape == expected_shape
 
-    # TODO: Issue #16
-    # single_predict_proba_pt = model_pt_adult.predict_proba(single_sample)
-    # assert single_predict_proba_pt.shape == expected_shape
-
     predictions_proba_tf = model_tf_adult.predict_proba(samples)
     expected_shape = tuple((22, 2))
     assert predictions_proba_tf.shape == expected_shape
-
-    # TODO: Issue #16
-    # predictions_proba_pt = model_pt_adult.predict_proba(samples)
-    # assert predictions_proba_pt.shape == expected_shape
