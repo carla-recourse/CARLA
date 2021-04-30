545c246a2d9e770f145b8b5975fbe5bbde113126
Philoso-Fish <sbielawski@web.de>
2021-04-30 15:51:48 +0200

    * Add loading of jit traced pytorch model * Change prediction methods for pytorch * Add tests

--- test/test_mlmodel.py
+++ test/test_mlmodel.py
@@ -1,4 +1,5 @@
 import numpy as np
+import torch
 
 from carla.data.catalog import DataCatalog
 from carla.models.catalog import MLModelCatalog
@@ -48,7 +49,7 @@ def test_properties():
     assert model_tf_adult.feature_input_order == exp_feature_order_adult
 
 
-def test_predictions():
+def test_predictions_tf():
     data_name = "adult"
     data_catalog = "adult_catalog.yaml"
     data = DataCatalog(data_name, data_catalog, drop_first_encoding=True)
@@ -82,11 +83,11 @@ def test_predictions():
 
     # Test single and bulk non probabilistic predictions
     single_prediction_tf = model_tf_adult.predict(single_sample)
-    expected_shape = tuple((1,))
+    expected_shape = tuple((1, 1))
     assert single_prediction_tf.shape == expected_shape
 
     predictions_tf = model_tf_adult.predict(samples)
-    expected_shape = tuple((22,))
+    expected_shape = tuple((22, 1))
     assert predictions_tf.shape == expected_shape
 
     # Test single and bulk probabilistic predictions
@@ -130,11 +131,11 @@ def test_predictions_with_pipeline():
 
     # Test single and bulk non probabilistic predictions
     single_prediction_tf = model_tf_adult.predict(single_sample)
-    expected_shape = tuple((1,))
+    expected_shape = tuple((1, 1))
     assert single_prediction_tf.shape == expected_shape
 
     predictions_tf = model_tf_adult.predict(samples)
-    expected_shape = tuple((22,))
+    expected_shape = tuple((22, 1))
     assert predictions_tf.shape == expected_shape
 
     # Test single and bulk probabilistic predictions
@@ -179,3 +180,87 @@ def test_pipeline():
 
     assert expected_shape == enc_norm_samples.shape
     assert enc_norm_samples.select_dtypes(exclude=[np.number]).empty
+
+
+def test_predictions_pt():
+    data_name = "adult"
+    data_catalog = "adult_catalog.yaml"
+    data = DataCatalog(data_name, data_catalog, drop_first_encoding=False)
+
+    feature_input_order = [
+        "age",
+        "fnlwgt",
+        "education-num",
+        "capital-gain",
+        "capital-loss",
+        "hours-per-week",
+        "sex_Female",
+        "sex_Male",
+        "workclass_Non-Private",
+        "workclass_Private",
+        "marital-status_Married",
+        "marital-status_Non-Married",
+        "occupation_Managerial-Specialist",
+        "occupation_Other",
+        "relationship_Husband",
+        "relationship_Non-Husband",
+        "race_Non-White",
+        "race_White",
+        "native-country_Non-US",
+        "native-country_US",
+    ]
+    model = MLModelCatalog(
+        data, "ann", feature_input_order, backend="pytorch", encode_normalize_data=True
+    )
+
+    single_sample = data.encoded_normalized.iloc[22]
+    single_sample = single_sample[model.feature_input_order].values.reshape((1, -1))
+    single_sample_torch = torch.Tensor(single_sample)
+
+    samples = data.encoded_normalized.iloc[0:22]
+    samples = samples[model.feature_input_order].values
+    samples_torch = torch.Tensor(samples)
+
+    # Test single non probabilistic predictions
+    single_prediction = model.predict(single_sample)
+    expected_shape = tuple((1, 1))
+    assert single_prediction.shape == expected_shape
+    assert isinstance(single_prediction, np.ndarray)
+
+    single_prediction_torch = model.predict(single_sample_torch)
+    expected_shape = tuple((1, 1))
+    assert single_prediction_torch.shape == expected_shape
+    assert torch.is_tensor(single_prediction_torch)
+
+    # bulk non probabilistic predictions
+    predictions = model.predict(samples)
+    expected_shape = tuple((22, 1))
+    assert predictions.shape == expected_shape
+    assert isinstance(predictions, np.ndarray)
+
+    predictions_torch = model.predict(samples_torch)
+    expected_shape = tuple((22, 1))
+    assert predictions_torch.shape == expected_shape
+    assert torch.is_tensor(predictions_torch)
+
+    # Test single probabilistic predictions
+    single_predict_proba = model.predict_proba(single_sample)
+    expected_shape = tuple((1, 2))
+    assert single_predict_proba.shape == expected_shape
+    assert isinstance(single_predict_proba, np.ndarray)
+
+    single_predict_proba_torch = model.predict_proba(single_sample_torch)
+    expected_shape = tuple((1, 2))
+    assert single_predict_proba_torch.shape == expected_shape
+    assert torch.is_tensor(single_predict_proba_torch)
+
+    # bulk probabilistic predictions
+    predictions_proba = model.predict_proba(samples)
+    expected_shape = tuple((22, 2))
+    assert predictions_proba.shape == expected_shape
+    assert isinstance(single_predict_proba, np.ndarray)
+
+    predictions_proba_torch = model.predict_proba(samples_torch)
+    expected_shape = tuple((22, 2))
+    assert predictions_proba_torch.shape == expected_shape
+    assert torch.is_tensor(predictions_proba_torch)
