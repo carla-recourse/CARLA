f8def3b44f92f0db664864ed83ed4ffcf70dc0f2
Sascha <sascha.bielawski@student.uni-tuebingen.de>
2021-05-06 15:35:57 +0200

    First structure for clue

---
+++ carla/recourse_methods/catalog/clue/model.py
@@ -0,0 +1,9 @@
+from carla.recourse_methods.api import RecourseMethod
+
+
+class Clue(RecourseMethod):
+    def __init__(self):
+        pass
+
+    def get_counterfactuals(self, factuals):
+        pass
