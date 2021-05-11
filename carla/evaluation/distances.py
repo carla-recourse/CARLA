d9cb84e7315e995345ec741ec60e2218d1406459
Sascha <sascha.bielawski@student.uni-tuebingen.de>
2021-05-11 16:15:15 +0200

    * Refactor distances * Add test cases

--- carla/evaluation/distances.py
+++ carla/evaluation/distances.py
@@ -1,170 +1,123 @@
 import numpy as np
 
 
-def get_distances(data, factual, counterfactual):
+def get_distances(factual, counterfactual):
     """
-    Computes distances 1 to 4
-    :param data: Dataframe with original data
-    :param factual: List of features
-    :param counterfactual: List of features
-    :return: Array of distances 1 to 4
-    """
-    d1 = d1_distance(factual, counterfactual)
-    d2 = d2_distance(factual, counterfactual, data)
-    d3 = d3_distance(factual, counterfactual, data)
-    d4 = d4_distance(factual, counterfactual)
+    Computes distances 1 to 4.
+    All features have to be in the same order (without target label).
 
-    return np.array([d1, d2, d3, d4])
+    Parameters
+    ----------
+    factual: np.ndarray
+        Normalized and encoded array with factual data.
+        Shape: NxM
+    counterfactual: np.ndarray
+        Normalized and encoded array with counterfactual data
+        Shape: NxM
 
+    Returns
+    -------
+    list: distances 1 to 4
+    """
+    if factual.shape != counterfactual.shape:
+        raise ValueError("Shapes of factual and counterfactual have to be the same")
+    if len(factual.shape) != 2:
+        raise ValueError(
+            "Shapes of factual and counterfactual have to be 2-dimensional"
+        )
 
-def d1_distance(instance, cf):
-    """
-    Compute d1-distance
-    :param instance: List of original feature
-    :param cf: List of counterfactual feature
-    :return: Scalar number
-    """
     # get difference between original and counterfactual
-    delta = get_delta(instance, cf)
+    delta = get_delta(factual, counterfactual)
 
+    d1 = d1_distance(delta)
+    d2 = d2_distance(delta)
+    d3 = d3_distance(delta)
+    d4 = d4_distance(delta)
+
+    return [d1, d2, d3, d4]
+
+
+def d1_distance(delta):
+    """
+    Computes D1 distance
+
+    Parameters
+    ----------
+    delta: np.ndarray
+        Difference between factual and counterfactual
+
+    Returns
+    -------
+    float
+    """
     # compute elements which are greater than 0
-    delta_bin = [i != 0 for i in delta]
-    delta_bin = delta_bin[:-1]  # loose label column
-
-    d1 = sum(delta_bin)
-
-    return d1
+    return np.sum(delta != 0)
 
 
-def d2_distance(instance, cf, df):
+def d2_distance(delta):
     """
-    Compute d2 distance
-    :param instance: List of original feature
-    :param cf: List of counterfactual feature
-    :param df: Dataframe object of dataset
-    :return: Scalar number
+    Computes D2 distance
+
+    Parameters
+    ----------
+    delta: np.ndarray
+        Difference between factual and counterfactual
+
+    Returns
+    -------
+    float
     """
-    # get difference between original and counterfactual
-    delta = get_delta(instance, cf)
-    delta = delta[:-1]  # loose label column
 
-    # get range of every feature
-    range = get_range(df)
-
-    d2 = [np.abs(x[0] / x[1]) for x in zip(delta, range)]
-    d2 = sum(d2)
-
-    return d2
+    return np.sum(np.abs(delta))
 
 
-def d3_distance(instance, cf, df):
+def d3_distance(delta):
     """
-    Compute d3 distance
-    :param instance: List of original feature
-    :param cf: List of counterfactual feature
-    :param df: Dataframe object of dataset
-    :return: Scalar number
+    Computes D3 distance
+
+    Parameters
+    ----------
+    delta: np.ndarray
+        Difference between factual and counterfactual
+
+    Returns
+    -------
+    float
     """
-    # get difference between original and counterfactual
-    delta = get_delta(instance, cf)
-    delta = delta[:-1]  # loose label column
-
-    # get range of every feature
-    range = get_range(df)
-
-    d3 = [(x[0] / x[1]) ** 2 for x in zip(delta, range)]
-    d3 = sum(d3)
-
-    return d3
+    return np.sum(np.square(np.abs(delta)))
 
 
-def d4_distance(instance, cf):
+def d4_distance(delta):
     """
-    Compute d4 distance
-    :param instance: List of original feature
-    :param cf: List of counterfactual feature
-    :return: Scalar number
+    Computes D4 distance
+
+    Parameters
+    ----------
+    delta: np.ndarray
+        Difference between factual and counterfactual
+
+    Returns
+    -------
+    float
     """
-    # get difference between original and counterfactual
-    delta = get_delta(instance, cf)
-    delta = delta[:-1]  # loose label column
-
-    d4 = [np.abs(x) for x in delta]
-    d4 = np.max(d4)
-
-    return d4
+    return np.max(np.abs(delta))
 
 
 def get_delta(instance, cf):
     """
     Compute difference between original instance and counterfactual
-    :param instance: List of features of original instance
-    :param cf: List of features of counterfactual
-    :return: List of differences between cf and original instance
+
+    Parameters
+    ----------
+    instance: np.ndarray
+        Normalized and encoded array with factual data.
+        Shape: NxM
+    cf: : np.ndarray
+        Normalized and encoded array with counterfactual data.
+        Shape: NxM
+
+    Returns
+    -------
+    np.ndarray
     """
-    delta = []
-    for i, original in enumerate(instance):
-        counterfactual = cf[i]
-
-        if type(original) == str:
-            if original == counterfactual:
-                delta.append(0)
-            else:
-                delta.append(1)
-        else:
-            delta.append(counterfactual - original)
-
-    return delta
-
-
-def get_max_list(data):
-    """
-    get max element for every column.
-    Max for string elements is 1
-    :param data: numpy array
-    :return: list of max elements
-    """
-    max = []
-    for i in range(data.shape[-1] - 1):
-        column = data[:, i]
-
-        if type(column[0]) == str:
-            max.append(1)
-        else:
-            max.append(np.max(column))
-
-    return max
-
-
-def get_min_list(data):
-    """
-    get min element for every column.
-    Min for string elements is 0
-    :param data: numpy array
-    :return: list of min elements
-    """
-    min = []
-    for i in range(data.shape[-1] - 1):
-        column = data[:, i]
-
-        if type(column[0]) == str:
-            min.append(0)
-        else:
-            min.append(np.min(column))
-
-    return min
-
-
-def get_range(df):
-    """
-    Get range max - min of every feature
-    :param df: dataframe object of dataset
-    :return: list of ranges for every feature
-    """
-    data = df.values
-    max = get_max_list(data)
-    min = get_min_list(data)
-
-    range = [x[0] - x[1] for x in zip(max, min)]
-
-    return range
+    return cf - instance
--- test/test_distances.py
+++ test/test_distances.py
@@ -1,60 +1,85 @@
+import numpy as np
+
 from carla import distances
 
 
 def test_d1():
-    actual = distances.d1_distance([0, 1], [0, 1])
+    test_input_1 = np.array([[0, 1]])
+    test_input_2 = np.array([[1, 0]])
+    test_input_3 = np.concatenate((test_input_1, test_input_2), axis=0)
+
+    delta = distances.get_delta(test_input_1, test_input_1)
+    actual = distances.d1_distance(delta)
     expected = 0
 
+    assert actual == expected
+
+    delta = distances.get_delta(test_input_1, test_input_2)
+    actual = distances.d1_distance(delta)
+    expected = 2
+
+    assert actual == expected
+
+    actual = distances.d1_distance(test_input_3)
+    assert actual == expected
+
+
+def test_d2():
+    test_input_1 = np.array([[0, 0]])
+    test_input_2 = np.array([[1, -1]])
+    test_input_3 = np.concatenate((test_input_1, test_input_2), axis=0)
+
+    expected = 0
+    actual = distances.d2_distance(test_input_1)
+    assert actual == expected
+
+    expected = 2
+    actual = distances.d2_distance(test_input_2)
+    assert actual == expected
+
+    actual = distances.d2_distance(test_input_3)
+    assert actual == expected
+
+
+def test_d3():
+    test_input_1 = np.array([[0, 0]])
+    test_input_2 = np.array([[1, -1]])
+    test_input_3 = np.concatenate((test_input_1, test_input_2), axis=0)
+
+    expected = 0
+    actual = distances.d3_distance(test_input_1)
+    assert actual == expected
+
+    expected = 2
+    actual = distances.d3_distance(test_input_2)
+    assert actual == expected
+
+    actual = distances.d3_distance(test_input_3)
     assert actual == expected
 
 
 def test_d4():
-    actual = distances.d4_distance([0, 1], [0, 1])
+    test_input_1 = np.array([[0, 0]])
+    test_input_2 = np.array([[1, -4]])
+    test_input_3 = np.concatenate((test_input_1, test_input_2), axis=0)
+
     expected = 0
+    actual = distances.d4_distance(test_input_1)
+    assert actual == expected
 
+    expected = 4
+    actual = distances.d4_distance(test_input_2)
+    assert actual == expected
+
+    actual = distances.d4_distance(test_input_3)
     assert actual == expected
 
 
-def test_counterfactuals():
+def test_distances():
 
-    factual = [
-        23,
-        "Private",
-        134446,
-        "HS-grade",
-        9,
-        "Separated",
-        "Machine-op-inpct",
-        "Unmarried",
-        "Black",
-        "Male",
-        0,
-        2356,
-        1,
-        "United States",
-    ]
+    test_input_1 = np.array([[1, 0, 1, 0, 0, 1, 1, 0], [1, 0, 1, 0, 0, 1, 1, 0]])
+    test_input_2 = np.array([[1, 0, 0, 0, 0, 1, 1, 0], [1, 0, 1, 0, 0, 1, 1, 0]])
 
-    counterfactual = [
-        40,
-        "Without-Pay",
-        132969,
-        "HS-grade",
-        9,
-        "Divorced",
-        "Machine-op-inpct",
-        "Unmarried",
-        "Black",
-        "Male",
-        98982,
-        3556,
-        1,
-        "United States",
-    ]
-
-    actual = distances.d1_distance(factual, counterfactual)
-    expected = 6
-    assert actual == expected
-
-    actual = distances.d4_distance(factual, counterfactual)
-    expected = 98982
+    expected = [1, 1, 1, 1]
+    actual = distances.get_distances(test_input_1, test_input_2)
     assert actual == expected
