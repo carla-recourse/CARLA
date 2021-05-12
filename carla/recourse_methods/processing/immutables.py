193da937d1a574b12773d8ae0d4b94f3d6c64053
Philoso-Fish <sbielawski@web.de>
2021-05-12 09:27:18 +0200

    Feature/add actionable recourse (#32)
    * * Restructure data and mlmodel to use correct scaler and encoder
    * Fix tests to new structure
    * Make pipeline more modular and add setter
    
    * Remove processing, since it is unnecessary after scaler and encoder properties
    
    * fix output to normalized and encoded
    
    * * Add loading of jit traced pytorch model
    * Change prediction methods for pytorch
    * Add tests
    
    * * Add Actionable Recourse method
    * Add tests
    
    * * Refactor immutable encoding
    * Add additional test cases
    
    * Rewriting iteration
    
    * Expand RecourseMethod with encoding and scaling function
    
    * Add local coeffs and intercepts to prevent sideeffects
    
    * Rename index variable to be more clearer
    
    * Refactor action testing for actionable recourse
    
    * Update carla/recourse_methods/catalog/actionable_recourse/model.py
    
    Co-authored-by: Tobias Richter <tr4711@gmail.com>
    
    * Add correct output variable
    
    * Fix bug for immutables for non drop-first-column models
    
    * * make action_set immutable
    
    * * Change method name of encoded_immutables
    
    Co-authored-by: Sascha <sascha.bielawski@student.uni-tuebingen.de>
    Co-authored-by: Tobias Richter <tr4711@gmail.com>

---
+++ carla/recourse_methods/processing/immutables.py
@@ -0,0 +1,18 @@
+def encode_feature_names(imtbls, input_order):
+    """
+    Transforms not encoded immutable feature names into encoded ones
+    Returns
+    -------
+    list
+    """
+    immutables = []
+    # TODO: Maybe find a more elegant way to find encoded immutable feature names
+    for feature in imtbls:
+        if feature in input_order:
+            immutables.append(feature)
+        else:
+            for cat in input_order:
+                if cat not in immutables:
+                    if feature in cat:
+                        immutables.append(cat)
+    return immutables
