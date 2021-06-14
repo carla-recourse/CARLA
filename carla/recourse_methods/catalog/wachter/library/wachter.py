d5e1bb9476157d5b60d61471acf92197f874e435
Philoso-Fish <sbielawski@web.de>
2021-06-14 15:43:32 +0200

    Refactor decision threshold as constant

--- carla/recourse_methods/catalog/wachter/library/wachter.py
+++ carla/recourse_methods/catalog/wachter/library/wachter.py
@@ -7,6 +7,8 @@ import torch.optim as optim
 from torch import nn
 from torch.autograd import Variable
 
+DECISION_THRESHOLD = 0.5
+
 
 def wachter_recourse(
     torch_model,
@@ -77,7 +79,7 @@ def wachter_recourse(
     t0 = datetime.datetime.now()
     t_max = datetime.timedelta(minutes=t_max_min)
 
-    while f_x_new <= 0.5:
+    while f_x_new <= DECISION_THRESHOLD:
         it = 0
         while f_x_new <= 0.5 and it < n_iter:
             optimizer.zero_grad()
