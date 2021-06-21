2f74a92b5bd08fbe28b20a329c5ef65c18d9f2a0
Sascha <sbielawski@web.de>
2021-06-21 15:02:09 +0200

    chores/hyperparameter check (#82)
    * * Add check for hyperparams keys and default values
    * Add test to check if at least one counterfactual is found
    
    * Refactor method name
    
    Co-authored-by: Sascha <sascha.bielawski@student.uni-tuebingen.de>

--- carla/recourse_methods/processing/__init__.py
+++ carla/recourse_methods/processing/__init__.py
@@ -3,6 +3,7 @@
 from .counterfactuals import (
     check_counterfactuals,
     get_drop_columns_binary,
+    merge_default_parameters,
     reconstruct_encoding_constraints,
 )
 from .immutables import encode_feature_names
