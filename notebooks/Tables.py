08f83f3d0ea6efb29c7b257dec108ac020b8eedb
Sascha <sascha.bielawski@student.uni-tuebingen.de>
2021-07-19 15:22:36 +0200

    Add standard error computation

--- notebooks/Tables.py
+++ notebooks/Tables.py
@@ -3,7 +3,9 @@
 # In[1]:
 
 
-from os import path
+import os
+from os import listdir
+from os.path import isfile, join
 
 import numpy as np
 import pandas as pd
@@ -14,8 +16,16 @@ import pandas as pd
 datasets = ["Adult", "COMPAS", "Give_Me_Some_Credit"]
 ml_methods = ["ANN", "Linear"]
 
-cf_ind = ["ar", "as", "cem", "dice", "gs"]
-cf_dep = ["ar-lime", "clue", "dice_vae", "cem-vae", "face-knn", "face-eps"]
+cf_ind = ["ar", "as", "cem", "dice", "gs", "wachter"]
+cf_dep = [
+    "ar-lime",
+    "clue",
+    "dice_vae",
+    "cem-vae",
+    "face-knn",
+    "face-epsilon",
+    "revise",
+]
 cf_methods = cf_ind + cf_dep
 
 measurements = [
@@ -36,25 +46,36 @@ measurements = [
 
 results = []
 
-for d in datasets:
-    for clf in ml_methods:
-        for cf in cf_methods:
-            filepath = f"../Results/{d}/{clf}/{cf}.csv"
+# for d in datasets:
+#     for clf in ml_methods:
+#         for cf in cf_methods:
+#             filepath = f"../Results/{d}/{clf}/{cf}.csv"
+#
+#             if path.exists(filepath):
+#                 df_result = pd.read_csv(filepath, index_col=0)
+#             else:
+#                 df_result = pd.DataFrame(
+#                     [[np.NaN] * len(measurements)], columns=measurements
+#                 )
+#
+#             df_result["data"] = d.lower()
+#             df_result["clf"] = clf.lower()
+#             df_result["cf_method"] = cf.lower().replace("-", "_")
+#
+#             results.append(df_result)
 
-            if path.exists(filepath):
-                df_result = pd.read_csv(filepath, index_col=0)
-            else:
-                df_result = pd.DataFrame(
-                    [[np.NaN] * len(measurements)], columns=measurements
-                )
+path = "C:/Users/sbiel/Downloads/results_for_csv"
+files = [f for f in listdir(path) if isfile(join(path, f))]
 
-            df_result["data"] = d.lower()
-            df_result["clf"] = clf.lower()
-            df_result["cf_method"] = cf.lower().replace("-", "_")
+for file in files:
+    df = pd.read_csv(os.path.join(path, file))
+    results.append(df)
+df = pd.concat(results).reset_index(drop=True)
+df["Recourse_Method"] = df["Recourse_Method"].apply(
+    lambda x: x.lower().replace("-", "_")
+)
 
-            results.append(df_result)
-
-df = pd.concat(results)
+# df = pd.concat(results)
 
 
 # In[11]:
@@ -70,25 +91,27 @@ def approach(method):
     return a
 
 
-df["approach"] = df["cf_method"].apply(approach)
+df["approach"] = df["Recourse_Method"].apply(approach)
 
 
 # In[21]:
 
 
-groupers = ["data", "cf_method", "clf"]
+groupers = ["Dataset", "Recourse_Method", "ML_Model"]
 
 df_agg = df.groupby(groupers, dropna=False, as_index=False).agg(
-    count=("ell0", "count"),
-    ell0=("ell0", "mean"),
-    ell1=("ell1", "mean"),
-    ell2=("ell2", "mean"),
-    ell_inf=("ell-inf", "mean"),
-    redundancy=("redundancy", "mean"),
-    violation=("violation", "mean"),
-    ynn=("ynn", "first"),
-    success=("success", "first"),
-    avgtime=("avgtime", "first"),
+    count=("Distance_1", "count"),
+    ell0=("Distance_1", "mean"),
+    ell1=("Distance_2", "mean"),
+    ell2=("Distance_3", "mean"),
+    ell_inf=("Distance_4", "mean"),
+    redundancy=("Redundancy", "mean"),
+    redundancy_std=("Redundancy", "std"),
+    violation=("Constraint_Violation", "mean"),
+    violation_std=("Constraint_Violation", "std"),
+    ynn=("y-Nearest-Neighbours", "first"),
+    success=("Success_Rate", "first"),
+    avgtime=("Average_Time", "first"),
     approach=("approach", "first"),
 )
 
@@ -106,7 +129,7 @@ a = df_agg.copy()
 df_agg = a
 
 datasets = {"give_me_some_credit": "GMC", "adult": "Adult", "compas": "COMPAS"}
-df_agg["data"] = df_agg["data"].replace(datasets)
+df_agg["Dataset"] = df_agg["Dataset"].replace(datasets)
 
 cf_methods = {
     "cem_vae": "\\texttt{CEM-VAE}",
@@ -120,7 +143,7 @@ cf_methods = {
     "dice": "\\texttt{DICE}",
     "gs": "\\texttt{GS}",
 }
-df_agg["cf_method"] = df_agg["cf_method"].replace(cf_methods)
+df_agg["Recourse_Method"] = df_agg["Recourse_Method"].replace(cf_methods)
 # df_agg["cf_method"] = df_agg["cf_method"].str.upper()
 
 df_agg["count"] = df_agg["count"].astype(int)
@@ -129,7 +152,9 @@ cols = {
     "count": "$n$",
     "ynn": "\textit{yNN}",
     "redundancy": "redund.",
+    "redundancy_std": "redund_std.",
     "violation": "violation",
+    "violation_std": "violation_std",
     "success": "success",
     "avgtime": "$\overline{t}(s)$",
 }
@@ -146,9 +171,13 @@ df_ind = df_agg[df_agg["approach"] == "independence"].drop("approach", axis="col
 
 def latex_table(df):
 
-    df = df.melt(id_vars=["data", "cf_method", "clf"], var_name="measurement")
+    df = df.melt(
+        id_vars=["Dataset", "Recourse_Method", "ML_Model"], var_name="measurement"
+    )
 
-    df = df.pivot_table(index=["data", "cf_method"], columns=["clf", "measurement"])
+    df = df.pivot_table(
+        index=["Dataset", "Recourse_Method"], columns=["ML_Model", "measurement"]
+    )
 
     df = df.T.reindex(cols.values(), level="measurement").T
 
@@ -166,7 +195,7 @@ latex_table(df_ind)
 # In[24]:
 
 
-df_latex = latex_table(df_ind)
+df_latex = latex_table(df_dep)
 latex = df_latex.to_latex(
     float_format="{:0.2f}".format,
     na_rep="--",
