26643a102018b499a0cd3826a0a720718facb9b6
Sascha <sascha.bielawski@student.uni-tuebingen.de>
2021-08-02 16:48:23 +0200

    removing unused information and print statements

--- carla/_logger.py
+++ carla/_logger.py
@@ -1,5 +1,2 @@
-DEBUGFORMATTER = "%(filename)s:%(name)s:%(funcName)s:%(lineno)d: %(message)s"
-"""Debug file formatter."""
-
 INFOFORMATTER = "%(message)s"
 """Log file and stream output formatter."""
