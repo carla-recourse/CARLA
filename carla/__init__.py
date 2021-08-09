e105bda14b10695db88d07a5efefb57e933b8936
Sascha <sascha.bielawski@student.uni-tuebingen.de>
2021-08-09 17:02:51 +0200

    Add different loggers possibility

--- carla/__init__.py
+++ carla/__init__.py
@@ -15,3 +15,7 @@ from .data import Data, DataCatalog
 from .evaluation import Benchmark
 from .models import MLModel, MLModelCatalog
 from .recourse_methods import RecourseMethod
+
+
+def get_logger(logger: str):
+    return logging.getLogger(logger)
