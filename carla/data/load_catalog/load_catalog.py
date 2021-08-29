ede6f84ecfe352d1bc651ef25eb90b25b76671a7
Tobias Richter <tr4711@gmail.com>
2021-08-29 19:17:09 +0200

    Improve error handling

--- carla/data/load_catalog/load_catalog.py
+++ carla/data/load_catalog/load_catalog.py
@@ -11,11 +11,12 @@ def load_catalog(filename: str, dataset: str, keys: List[str]):
         catalog = yaml.safe_load(f)
 
     if dataset not in catalog:
-        raise KeyError("Dataset not in catalog.")
+        raise KeyError(f"Dataset '{dataset}' not in catalog.")
 
+    # TODO: Use schema validation instead of passing required keys
     for key in keys:
         if key not in catalog[dataset].keys():
-            raise KeyError("Important key {} is not in Catalog".format(key))
+            raise KeyError(f"Required key {key} is not in Catalog")
         if catalog[dataset][key] is None:
             catalog[dataset][key] = []
 
