0e480bc8824afffc61279439965fcb0252630fc4
Johan van den Heuvel <johanheuvel5@gmail.com>
2021-12-30 14:05:33 +0100

    predict negative instances now works for either a model with use_pipeline True or False. (#119)

--- carla/models/negative_instances/predict.py
+++ carla/models/negative_instances/predict.py
@@ -7,7 +7,9 @@ from carla.data.api import Data
 from carla.models.pipelining import encode, scale
 
 
-def predict_negative_instances(model: Any, data: Data) -> pd.DataFrame:
+def predict_negative_instances(
+    model: Any, data: Data, normalize: bool = True
+) -> pd.DataFrame:
     """Predicts the data target and retrieves the negative instances. (H^-)
 
     Assumption: Positive class label is at position 1
@@ -18,19 +20,23 @@ def predict_negative_instances(model: Any, data: Data) -> pd.DataFrame:
         Model object retrieved by :func:`load_model`
     data : Data
         Dataset used for predictions
+    normalize : bool
+        Flag if the data should be normalized. This should be set to False if the model has use_pipeline set to True.
     Returns
     -------
     df :  data.api Data() class with negative predicted instances
     """
     df = data.raw
-    df["y"] = predict_label(model, data)
+    df["y"] = predict_label(model, data, normalize=normalize)
     df = df[df["y"] == 0]
     df = df.drop("y", axis="columns")
 
     return df
 
 
-def predict_label(model: Any, data: Data, as_prob: bool = False) -> np.ndarray:
+def predict_label(
+    model: Any, data: Data, as_prob: bool = False, normalize=True
+) -> np.ndarray:
     """Predicts the data target
 
     Assumption: Positive class label is at position 1
@@ -46,13 +52,21 @@ def predict_label(model: Any, data: Data, as_prob: bool = False) -> np.ndarray:
     predictions :  2d numpy array with predictions
     """
 
-    # normalize and encode data
-    norm_enc_data = scale(model.scaler, data.continous, data.raw)
-    norm_enc_data = encode(model.encoder, data.categoricals, norm_enc_data)
+    if normalize:
+        # normalize and encode data
+        norm_enc_data = scale(model.scaler, data.continous, data.raw)
+        norm_enc_data = encode(model.encoder, data.categoricals, norm_enc_data)
 
-    # Keep correct feature order for prediction
-    norm_enc_data = norm_enc_data[model.feature_input_order]
-    predictions = model.predict(norm_enc_data)
+        # Keep correct feature order for prediction
+        norm_enc_data = norm_enc_data[model.feature_input_order]
+        predictions = model.predict(norm_enc_data)
+    else:
+        if not model.use_pipeline:
+            # Need to drop target column
+            norm_enc_data = data.raw[model.feature_input_order]
+            predictions = model.predict(norm_enc_data)
+        else:
+            predictions = model.predict(data.raw)
 
     if not as_prob:
         predictions = predictions.round()
