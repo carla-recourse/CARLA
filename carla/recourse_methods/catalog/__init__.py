a5612125c60a03b9ff966fa01c4083d6749c3816
Johan van den Heuvel <johanheuvel5@gmail.com>
2022-03-10 14:21:20 +0100

    Chore/update init recoursemodels (#138)
    * updated init with new methods
    
    * fixed import

--- carla/recourse_methods/catalog/__init__.py
+++ carla/recourse_methods/catalog/__init__.py
@@ -1,12 +1,15 @@
 # flake8: noqa
 
 from .actionable_recourse import ActionableRecourse
+from .causal_recourse import CausalRecourse
 from .cchvae import CCHVAE
 from .cem import CEM
 from .clue import Clue
 from .crud import CRUD
 from .dice import Dice
 from .face import Face
+from .feature_tweak import FeatureTweak
+from .focus import FOCUS
 from .growing_spheres import GrowingSpheres
 from .revise import Revise
 from .wachter import Wachter
