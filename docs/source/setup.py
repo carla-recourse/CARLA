42831656f24adba9eec3be56a7d2c77d16e62faa
JohanvandenHeuvel <johanheuvel5@gmail.com>
2022-03-16 11:52:18 +0100

    added pillow to requirements

--- docs/source/setup.py
+++ docs/source/setup.py
@@ -20,6 +20,7 @@ setup(
         "dice-ml==0.5",
         "ipython",
         "keras==2.3.0",
+        "pillow==9.0.1",
         "matplotlib==3.5.1",
         "networkx==2.5.1",
         "imageio==2.9.0",
