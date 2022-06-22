07d07be15dcf7617d0549a18f49b69af572259dd
JohanvandenHeuvel <johanheuvel5@gmail.com>
2022-06-22 16:11:39 +0200

    fixed import ordering

--- carla/evaluation/__init__.py
+++ carla/evaluation/__init__.py
@@ -1,12 +1,4 @@
 # flake8: noqa
 
 from .benchmark import Benchmark
-from .catalog import (
-    YNN,
-    AvgTime,
-    ConstraintViolation,
-    Distance,
-    Redundancy,
-    SuccessRate,
-)
 from .process_nans import remove_nans
