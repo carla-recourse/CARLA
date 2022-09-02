57d7732ca29b52c92eb9f5e81bd7512dd6aa10e5
Annabelle Redelmeier <aredelmeier@gmail.com>
2022-09-02 21:20:56 +0200

    update scaling and encoding to take in a function WITH TESTS

--- carla/data/pipelining/transformers.py
+++ carla/data/pipelining/transformers.py
@@ -7,7 +7,8 @@ def fit_scaler(scaling_method, df):
     Parameters
     ----------
     scaling_method: {"MinMax", "Standard", "Identity"}
-        String indicating what scaling method to use.
+        String indicating what scaling method to use or
+        sklearn.preprocessing function.
     df: pd.DataFrame
         DataFrame only containing continuous data.
 
@@ -16,15 +17,19 @@ def fit_scaler(scaling_method, df):
     sklearn.base.BaseEstimator
 
     """
-    # TODO rather then passing a string this could accept a function
-    if scaling_method == "MinMax":
-        fitted_scaler = preprocessing.MinMaxScaler().fit(df)
-    elif scaling_method == "Standard":
-        fitted_scaler = preprocessing.StandardScaler().fit(df)
-    elif scaling_method is None or "Identity":
-        fitted_scaler = preprocessing.FunctionTransformer(func=None, inverse_func=None)
-    else:
-        raise ValueError("Scaling Method not known")
+    if isinstance(scaling_method, str):
+        if scaling_method == "MinMax":
+            fitted_scaler = preprocessing.MinMaxScaler().fit(df)
+        elif scaling_method == "Standard":
+            fitted_scaler = preprocessing.StandardScaler().fit(df)
+        elif scaling_method is None or "Identity":
+            fitted_scaler = preprocessing.FunctionTransformer(
+                func=None, inverse_func=None
+            )
+        else:
+            raise ValueError("Scaling Method not known")
+    elif hasattr(scaling_method, "fit"):  # check if function has fit attribute
+        fitted_scaler = scaling_method.fit(df)
     return fitted_scaler
 
 
@@ -34,7 +39,8 @@ def fit_encoder(encoding_method, df):
     Parameters
     ----------
     encoding_method: {"OneHot", "OneHot_drop_binary", "Identity"}
-        String indicating what encoding method to use.
+        String indicating what encoding method to use or
+        sklearn.preprocessing function.
     df: pd.DataFrame
         DataFrame containing only categorical data.
 
@@ -43,17 +49,20 @@ def fit_encoder(encoding_method, df):
     sklearn.base.BaseEstimator
 
     """
-    # TODO rather then passing a string this could accept a function
-    if encoding_method == "OneHot":
-        fitted_encoder = preprocessing.OneHotEncoder(
-            handle_unknown="error", sparse=False
-        ).fit(df)
-    elif encoding_method == "OneHot_drop_binary":
-        fitted_encoder = preprocessing.OneHotEncoder(
-            drop="if_binary", handle_unknown="error", sparse=False
-        ).fit(df)
-    elif encoding_method is None or "Identity":
-        fitted_encoder = preprocessing.FunctionTransformer(func=None, inverse_func=None)
-    else:
-        raise ValueError("Encoding Method not known")
+    if isinstance(encoding_method, str):
+        if encoding_method == "OneHot":
+            fitted_encoder = preprocessing.OneHotEncoder(
+                handle_unknown="error", sparse=False
+            ).fit(df)
+        elif encoding_method == "OneHot_drop_binary":
+            fitted_encoder = preprocessing.OneHotEncoder(
+                drop="if_binary", handle_unknown="error", sparse=False
+            ).fit(df)
+        elif encoding_method is None or "Identity":
+            fitted_encoder = preprocessing.FunctionTransformer(
+                func=None, inverse_func=None
+            )
+
+    elif hasattr(encoding_method, "fit"):  # check if function has fit attribute
+        fitted_encoder = encoding_method.fit(df)
     return fitted_encoder
--- test/test_data.py
+++ test/test_data.py
@@ -1,5 +1,6 @@
 import pytest
-
+from pandas._testing import assert_frame_equal
+from sklearn import preprocessing
 from carla.data.catalog import OnlineCatalog
 
 testdata = ["adult", "give_me_some_credit", "compas", "heloc"]
@@ -17,3 +18,49 @@ def test_adult_col(data_name):
     expected_col = expected_col.sort()
 
     assert actual_col == expected_col
+
+def test_transform():
+    data_name = "adult"
+    raw_data = OnlineCatalog(
+        data_name, scaling_method="Identity", encoding_method="Identity"
+    )
+    transformed_data_str = OnlineCatalog(
+        data_name, scaling_method="MinMax", encoding_method="OneHot_drop_binary"
+    )
+    
+    encoding_method = preprocessing.OneHotEncoder(
+            handle_unknown="error", sparse=False
+    )
+    transformed_data_fn = OnlineCatalog(
+        data_name, scaling_method="MinMax", encoding_method=encoding_method
+    )
+    # sort columns as order could be different
+    assert_frame_equal(
+        transformed_data_str.inverse_transform(transformed_data_str.df).sort_index(axis=1),
+        raw_data.df.sort_index(axis=1),
+        check_dtype=False,
+    )
+    assert_frame_equal(
+        transformed_data_str.transform(raw_data.df).sort_index(axis=1),
+        transformed_data_str.df.sort_index(axis=1),
+        check_dtype=False,
+    )
+    assert_frame_equal(
+        transformed_data_str.transform(
+            transformed_data_str.inverse_transform(transformed_data_str.df)
+        ).sort_index(axis=1),
+        transformed_data_str.df.sort_index(axis=1),
+        check_dtype=False,
+    )
+    assert_frame_equal(
+        transformed_data_str.inverse_transform(
+            transformed_data_str.transform(raw_data.df)
+        ).sort_index(axis=1),
+        raw_data.df.sort_index(axis=1),
+        check_dtype=False,
+    )
+    assert_frame_equal(
+        transformed_data_str.inverse_transform(transformed_data_str.df).sort_index(axis=1),
+        transformed_data_fn.inverse_transform(transformed_data_fn.df).sort_index(axis=1),
+        check_dtype=False,
+    )
